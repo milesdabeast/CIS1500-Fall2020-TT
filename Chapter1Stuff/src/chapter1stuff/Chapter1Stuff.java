4f01f86a9925a383b10ec400c4d072b66c559ea4
Eric Charnesky <eric.charnesky@careevolution.com>
2020-09-03 11:14:37 -0400

    project created

---
+++ Chapter1Stuff/src/chapter1stuff/Chapter1Stuff.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chapter1stuff;
+
+/**
+ *
+ * @author EricC
+ */
+public class Chapter1Stuff {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
